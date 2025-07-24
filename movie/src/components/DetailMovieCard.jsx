0b215e939443f0c974e207b460fdc7530dedb9c4
hyeonseo1109 <alsgustj2@naver.com>
2025-07-24 19:15:39 +0900

    design: 상세페이지 css 수정

--- movie/src/components/DetailMovieCard.jsx
+++ movie/src/components/DetailMovieCard.jsx
@@ -54,13 +54,31 @@ export function DetailMovieCard () {
     <div className="flex flex-wrap gap-4 p-4 mx-15 my-5">
             <div key={detailMovies.id} className="bg-black shadow-[0_0_15px_#000000c1] text-white detail">
                 <div className="flex flex-row gap-4">
-                    {detailMovies.genres.map((gr) => <div className="text-[#999] shadow-[0_0_10px_rgb(255,255,255,0.]">#{gr.name}</div>)}
+                    {detailMovies.genres.map((gr) => <div className="text-[#000] bg-[#555] rounded-[0.3em] text-[0.95em]">#{gr.name}</div>)}
                 </div>
-                <div className="break-keep leading-8 tracking-tight text-left p-5 flex gap-5">
-                    <img
-                        className="aspect-[2/3] max-w-full h-auto"
-                        src={`https://image.tmdb.org/t/p/w500/xoYc0RYKSc3xC4S9OpPZxKocKtj.jpg`} />
-                    {detailMovies.overview}
+                <div>
+                    <div className="relative w-[40em] mx-auto">
+                        <img
+                            className="w-[40em] h-auto"
+                            alt="배경이미지"
+                            src={`https://image.tmdb.org/t/p/w500/uDosHOFFWtF5YteBRygHALFqLw2.jpg`} />
+                        <div 
+                            className="
+                            bg-[linear-gradient(180deg,rgb(255,255,255,0)10%,rgb(255,255,255,0.9))]
+                            absolute top-[7.5em] left-0 w-full h-[15em]
+                            "
+                            alt="흰박스"
+                        ></div>
+
+                    </div>
+                    <div className="break-keep leading-8 tracking-tight text-left p-5 flex gap-5">
+                        <img
+                            alt="작은사진"
+                            className="aspect-[2/3] w-[10em] h-auto"
+                            src={`https://image.tmdb.org/t/p/w500/xoYc0RYKSc3xC4S9OpPZxKocKtj.jpg`} />
+                        {detailMovies.overview}
+                    </div>
+
                 </div>
             </div>
     </div>
